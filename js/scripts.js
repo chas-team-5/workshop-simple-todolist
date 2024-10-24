f83a28ba50ddf3e9efb76af7e84b14d2a4e08209
Mary Pope <marypope19@gmail.com>
2024-10-24 10:31:54 +0200

    add a to-do

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-let todos = [];
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.getElementById('submitButton');
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    const todos = [];
+    let newId = 0;
 
-console.log("Hello World!");
+    submitButton.addEventListener('click', addTask);
+
+    function addTask(event) {
+        event.preventDefault();
+        newId++;
+
+        const newTask = {
+          id: newId,
+          task: taskInput.value,
+          completed: false
+        }
+
+        todos.push(newTask);
+        renderList(todos);
+    }
+
+    function renderList(todos) {
+      taskList.innerHTML = '';
+      
+      todos.forEach(function(item) {
+        const li = document.createElement("li");
+
+        li.textContent = item.task;
+        taskList.appendChild(li);
+      });
+
+      console.log("LOG TODOS: ", todos)
+    }   
+});
